04e43880c13bf222201ffaf46140f8cde2a264c9
Ivan <kombat_kos@mail.ru>
2019-10-21 12:18:21 +0400

    Initial Commit

---
+++ UiKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by kot on 21/10/2019.
+//  Copyright © 2019 Konstantin Porokhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
